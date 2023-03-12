d8edc3e2183229ad1b0da492911e03d79d2ae5c1
ReshmaShrema <shremareshma@gmail.com>
2023-03-12 15:18:23 +0530

    add custom css

--- Client/pages/index.js
+++ Client/pages/index.js
@@ -1,7 +1,9 @@
 const Index =()=>{
-    return(
-        <><h1 className="text-center bg-primary">Hellooo</h1>
-        <p>This from next js</p></>
-    )
+    return (
+        <>
+            <h1 className="jumbotron text-center bg-primary">Hellooo</h1>
+            <p>This from next js</p>
+        </>
+    );
 }
 export default Index;
